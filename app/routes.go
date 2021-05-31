594a92c4cfff457ed7309c69e836effbfa03e883
Premkumar Nagarajan <prem@Premkumars-MacBook-Pro.local>
2021-05-31 16:43:23 +0530

    [Go]: added promethus metrics

--- app/routes.go
+++ app/routes.go
@@ -1,15 +1,25 @@
 package app
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/premsvmm/goapi/controllers"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func routes() {
 	Router.GET("", controllers.Welcome)
 	Router.GET("/hostname", controllers.Hostname)
 	Router.POST("/add",controllers.Addition)
+	Router.GET("/metrics", prometheusHandler())
 }
 
+func prometheusHandler() gin.HandlerFunc {
+	h := promhttp.Handler()
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
 func webRoutes(){
 	Router.GET("/home",controllers.Webview)
 }
--- go.mod
+++ go.mod
@@ -5,10 +5,9 @@ go 1.13
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.2
-	github.com/golang/protobuf v1.4.0 // indirect
 	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.10.0
 	github.com/stretchr/testify v1.5.1
-	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
 )
